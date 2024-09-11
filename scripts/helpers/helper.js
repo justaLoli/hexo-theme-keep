0ab1442707c2d4f1a486abbcb36decc8fa086a46
justaLoli <huangziyu43@gmail.com>
2024-09-11 16:15:48 +0800

    update the fork repo to what i'm using rn

--- scripts/helpers/helper.js
+++ scripts/helpers/helper.js
@@ -2,14 +2,6 @@
 
 'use strict'
 
-hexo.extend.helper.register('isInHomePaging', function (pagePath, route) {
-  if (pagePath.length > 5 && route === '/') {
-    return pagePath.slice(0, 5) === 'page/'
-  } else {
-    return false
-  }
-})
-
 hexo.extend.helper.register('createNewArchivePosts', function (posts) {
   const postList = [],
     postYearList = []
@@ -28,13 +20,15 @@ hexo.extend.helper.register('createNewArchivePosts', function (posts) {
   return postList
 })
 
-hexo.extend.helper.register('getAuthorLabel', function (postCount, authorLabelConfig) {
-  const { auto: isAuto, custom_label_list: labelList } = authorLabelConfig || {}
+hexo.extend.helper.register('getAuthorBadge', function (postCount, authorLabelConfig) {
+  const { level_badge: isAuto, custom_badge: customBadge } = authorLabelConfig || {}
   let level = Math.floor(Math.log2(postCount))
   level = level < 2 ? 1 : level - 1
 
-  if (isAuto === false && Array.isArray(labelList) && labelList.length > 0) {
-    return level > labelList.length ? labelList[labelList.length - 1] : labelList[level - 1]
+  if (isAuto === false && Array.isArray(customBadge) && customBadge.length > 0) {
+    return level > customBadge.length ? customBadge[customBadge.length - 1] : customBadge[level - 1]
+  } else if (isAuto === false && typeof customBadge === 'string' && customBadge !== '') {
+    return customBadge
   } else {
     return `Lv${level}`
   }
@@ -43,24 +37,43 @@ hexo.extend.helper.register('getAuthorLabel', function (postCount, authorLabelCo
 const getSourceCdnUrl = (tyle, themeConfig, path) => {
   const version = require('../../package.json').version
   let { provider } = themeConfig?.cdn || {}
-  if (!provider) {
-    provider = 'jsdelivr'
+  const providerEnum = {
+    jsdelivr: 'jsdelivr',
+    unpkg: 'unpkg',
+    cdnjs: 'cdnjs'
   }
+
+  if (!provider) {
+    provider = providerEnum.cdnjs
+  }
+
   let urlPrefix = ''
+
   switch (provider?.toLocaleLowerCase()) {
-    case 'jsdelivr':
+    case providerEnum.jsdelivr:
       urlPrefix = '//cdn.jsdelivr.net/npm/hexo-theme-keep'
       if (tyle === 'js') {
         return `<script src="${urlPrefix}@${version}/source/${path}"></script>`
       } else {
         return `<link rel="stylesheet" href="${urlPrefix}@${version}/source/${path}">`
       }
-    case 'unpkg':
+
+    case providerEnum.unpkg:
       urlPrefix = '//unpkg.com/hexo-theme-keep'
       if (tyle === 'js') {
         return `<script src="${urlPrefix}@${version}/source/${path}"></script>`
       } else {
         return `<link rel="stylesheet" href="${urlPrefix}@${version}/source/${path}">`
+      }
+
+    case providerEnum.cdnjs:
+      urlPrefix = 'https://cdnjs.cloudflare.com/ajax/libs/hexo-theme-keep'
+      if (tyle === 'js') {
+        path = path.includes('.min.js') ? path : path.replace('.js', '.min.js')
+        return `<script src="${urlPrefix}/${version}/${path}"></script>`
+      } else {
+        path = path.includes('.min.css') ? path : path.replace('.css', '.min.css')
+        return `<link rel="stylesheet" href="${urlPrefix}/${version}/${path}">`
       }
   }
 }
@@ -97,12 +110,4 @@ hexo.extend.helper.register('isJsFile', function (path) {
 
 hexo.extend.helper.register('isCssFile', function (path) {
   return /\.css$/i.test(path)
-})
-
-hexo.extend.helper.register('isLinksPage', function (pagePath) {
-  return pagePath === 'links/index.html'
-})
-
-hexo.extend.helper.register('isCategoriesPage', function (pagePath) {
-  return pagePath === 'categories/index.html'
 })
